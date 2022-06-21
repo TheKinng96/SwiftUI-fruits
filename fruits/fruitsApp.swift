9eeaf41195529adfa4de6dadc43aab544ed7d435
G3N <fengyuan.yap@gmail.com>
2022-06-21 21:32:35 +0900

    Initial Commit

---
+++ fruits/fruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitsApp.swift
+//  fruits
+//
+//  Created by Feng Yuan Yap on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct fruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
